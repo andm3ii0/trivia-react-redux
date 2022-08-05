a2659eddb4bc7fdaff1a00674f339b42fbd1b0d8
Anderson Mello <andersonmello722@gmail.com>
2022-08-05 17:29:18 -0300

    Requisito 6

---
+++ src/redux/actions/index.js
@@ -0,0 +1,29 @@
+export const FETCH_QUESTIONS = 'FETCH_QUESTIONS';
+export const REQUEST_API = 'REQUEST_API';
+export const REQUEST_SUCSSES = 'REQUEST_SUCSSES';
+export const REQUEST_ERROR = 'REQUEST_ERROR';
+
+const requestAPI = () => ({
+  type: REQUEST_API,
+});
+
+const requestSuccess = (obj) => ({
+  type: REQUEST_SUCSSES,
+  questions: obj.results,
+});
+
+const requestError = () => ({
+  type: REQUEST_ERROR,
+});
+
+export const requestQuestions = (endPoint) => async (dispatch) => {
+  dispatch(requestAPI());
+  try {
+    const resolve = await fetch(endPoint);
+    const data = await resolve.json();
+    console.log(data);
+    dispatch(requestSuccess(data));
+  } catch (error) {
+    dispatch(requestError());
+  }
+};
