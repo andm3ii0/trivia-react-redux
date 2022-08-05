64ee23ec0989de20f19a06afb7163d8d982a1e48
Camila <falaschi.camila@yahoo.com.br>
2022-08-05 15:24:49 -0300

    criando inicial redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import store from './redux/store/index';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>, //
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </BrowserRouter>,
 
   document.getElementById('root'),
 );
