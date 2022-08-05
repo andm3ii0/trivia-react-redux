1a8ca70cc720b4520b2fbe74ee87669d3ddebc29
Camila <falaschi.camila@yahoo.com.br>
2022-08-05 15:28:37 -0300

    criando estado inicial

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,4 +1,9 @@
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  assertions: '',
+  score: 0,
+  gravatarEmail: '',
+};
 
 const player = (state = INITIAL_STATE, action) => {
   switch (action.type) {
