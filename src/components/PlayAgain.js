1c226b2e80feaf52a2764ad0f088853765ba06a1
FabioSC <fabioscarvalh@yahoo.com.br>
2022-08-08 18:17:41 -0300

    requisito 15 botão play again

---
+++ src/components/PlayAgain.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+class PlayAgain extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      redirect: false,
+    };
+  }
+
+  playAgain = () => {
+    this.setState({ redirect: true });
+  }
+
+  render() {
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <button
+        type="button"
+        data-testid="btn-play-again"
+        onClick={ this.playAgain }
+      >
+        Play Again
+      </button>
+    );
+  }
+}
+
+export default PlayAgain;
