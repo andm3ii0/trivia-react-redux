5610b37517a4a70dfeb58cb6de2311a1ccf2464c
FabioSC <fabioscarvalh@yahoo.com.br>
2022-08-09 19:10:17 -0300

    requisito 20 teste ranking atualizado

--- src/tests/Ranking.spec.js
+++ src/tests/Ranking.spec.js
@@ -1,8 +1,9 @@
 import { screen } from '@testing-library/dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import Ranking from '../pages/Ranking';
-// import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 
 const object1 = {email: 'user@test.com', score: 134, name: 'UserTest'};
 const object2 = {email: "test@user.com", score: 56, name: "TestUser"};
@@ -16,11 +17,26 @@ localStorage.setItem('ranking', ranking);
 describe('Ranking page', () => {
   it('ranking to be on the screen', async () => {
     render(<Ranking />);
-    const rankingTitle = screen.getByTestId('ranking-title');
+    const rankingTitle = screen.getByTestId('ranking-title');    
+    expect(rankingTitle).toBeInTheDocument();
+  });
+  it('first place to be higher then second', async () => {
+    render(<Ranking />);
     const rankingFirst = screen.getByTestId('player-score-0');
     const rankingSecond = screen.getByTestId('player-score-1');
-    expect(rankingTitle).toBeInTheDocument();
+    const first = JSON.parse(rankingFirst.innerHTML);
+    const second = JSON.parse(rankingSecond.innerHTML);
+    const higher = first > second;
     expect(rankingFirst).toBeInTheDocument();
     expect(rankingSecond).toBeInTheDocument();
+    expect(higher).toBe(true);
+  });
+  it('to have a button to Home', async () => {
+    const { history } = renderWithRouterAndRedux(<Ranking />);
+    const { pathname } = history.location;
+    const button = screen.getByTestId('btn-go-home');
+    expect(button).toBeInTheDocument();
+    userEvent.click(button);
+    expect(pathname).toBe('/');
   });
 });
