98686d0524fe9385de7fbfff15624014d379a451
Camila <falaschi.camila@yahoo.com.br>
2022-08-09 16:31:41 -0300

    adicionando describe

--- src/tests/Page-game.spec.js
+++ src/tests/Page-game.spec.js
@@ -1,6 +1,12 @@
-import { screen } from "@testing-library/dom";
-import { waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import React from "react";
-import App from "../App";
-import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
+import { screen } from '@testing-library/dom';
+import { waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+
+describe('Game page', () => {
+  it('should ', () => {
+
+  });
+});
